601b4ad85d1c8dfdda501c891b5035960c94cf11
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2023-10-18 10:25:16 +0000

    adding utils module to prometheus_metric

--- prometheus_metric.py
+++ prometheus_metric.py
@@ -1,61 +1,91 @@
-#!/usr/bin/env python3
 import requests
+import os
 import sys
 import yaml
+from utils import insert_into_table
+
 
 # Check if the correct number of command-line arguments is provided
 if len(sys.argv) != 2:
     print("Usage: python script.py <yaml_file>")
     sys.exit(1)
 
-# Get the YAML file path from the command-line argument
-yaml_file_path = sys.argv[1]
-
-# Read the contents of the specified YAML file
-try:
-    with open(yaml_file_path, 'r') as file:
-        yaml_data = file.read()
-except FileNotFoundError:
-    print(f"Error: File '{yaml_file_path}' not found.")
+folder_path = sys.argv[1]  # Get the folder path from the command line
+if not os.path.isdir(folder_path):
+    print(f"Error: {folder_path} is not a valid directory.")
     sys.exit(1)
 
-# Parse the YAML data
-try:
-    data = yaml.safe_load(yaml_data)
-except yaml.YAMLError as e:
-    print(f"Error parsing YAML file: {e}")
-    sys.exit(1)
 
-# Extract variables from the 'spec' element
-spec = data.get('spec', {})
+for filename in os.listdir(folder_path):
+    if filename.endswith('.yaml'):
+        yaml_file_path = os.path.join(folder_path, filename)
 
-# Get all keys in 'spec' before 'metric'
-variables = {}
-for key, value in spec.items():
-    if key == 'metric':
-        query = value
-        break
-    variables[key] = value
+        # Read the contents of the YAML file
+        try:
+            with open(yaml_file_path, 'r') as file:
+                yaml_data = file.read()
+        except FileNotFoundError:
+            print(f"Error: File '{yaml_file_path}' not found.")
+            continue
 
-# Replace placeholders in 'query' with corresponding variables
-for key, value in variables.items():
-    placeholder = f'{{{key}}}'
-    query = query.replace(placeholder, f'{{{value}}}')
+        # Parse the YAML data
+        try:
+            data = yaml.safe_load(yaml_data)
+        except yaml.YAMLError as e:
+            print(f"Error parsing YAML file: {e}")
+            continue
+
+        # Extract variables from the 'spec' element
+        spec = data.get('spec', {})
+
+        # Get all keys in 'spec' before 'metric'
+        variables = {}
+        for key, value in spec.items():
+            if key == 'metric':
+                query = value
+                break
+            variables[key] = value
+
+        # Replace placeholders in 'query' with corresponding variables
+        for key, value in variables.items():
+            placeholder = f'{{{key}}}'
+            query = query.replace(placeholder, f'{{{value}}}')
+
+        timeframe = "2m"
 
 
-# Define the Prometheus query expression
-query_expression = 'go_memstats_alloc_bytes{job="prometheus"}[2m]'#query#'process_cpu_seconds_total{job="prometheus"}[2m]'
-#your_metric
-#cpu = "1"
-#memory = "1"
-#price = "0.5"
-timeframe = "2m"
+        #query_expression = f'{query}{{job="prometheus"}}[{timeframe}]'
+        query_expression = query
+        print(query)
+        #query_expression = query_expression+'{job="prometheus"}'+f'[{timeframe}]'
+        #print(query_expression)
+
+        # Define the Prometheus API URL
+        prometheus_url = 'http://localhost:9090/api/v1/query'
+
+        # Construct the request parameters
+        params = {
+
+            'query' : query_expression,
+
+        }
+
+        # Send the HTTP GET request to Prometheus
+        response = requests.get(prometheus_url, params=params)
+
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            result = response.json()
+            # Process the query result as needed
+            print(result)
+            table_name = 'sensor_data'
+            insert_into_table(table_name, result['value'])
+            
+        else:
+            print(f"Failed to execute query. Status code: {response.status_code}")
 
 
 
-
-#query_expression = query_expression+'{job="prometheus"}'+f'[{timeframe}]'
-print(query_expression)
 
 # Define the Prometheus API URL
 prometheus_url = 'http://localhost:9090/api/v1/query'
---
+++ utils.py
@@ -0,0 +1,87 @@
+import sys
+import yaml
+import psycopg2
+
+def insert_into_table(table_name, values):
+    # Define your PostgreSQL database connection parameters
+    db_params = {
+        'dbname': 'mydatabase',
+        'user': 'myuser',
+        'password': 'mypassword',
+        'host': 'localhost'  # Typically 'localhost' for local connections
+        #'port': 'your_port'   # Default PostgreSQL port is 5432
+    }
+
+    # Connect to the PostgreSQL database
+    try:
+        conn = psycopg2.connect(**db_params)
+    except psycopg2.Error as e:
+        print(f"Error: Unable to connect to the database: {e}")
+        sys.exit(1)
+
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
+
+    try:
+        # Construct the SQL INSERT statement dynamically
+        insert_sql = f"INSERT INTO {table_name} VALUES ({', '.join(['%s'] * len(values))});"
+        
+        # Execute the INSERT statement with the provided values
+        cursor.execute(insert_sql, values)
+        
+        # Commit the transaction to save the changes
+        conn.commit()
+        print("Data inserted successfully.")
+    except psycopg2.Error as e:
+        # Rollback the transaction in case of an error
+        conn.rollback()
+        print(f"Error: Unable to insert data: {e}")
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        conn.close()
+
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python script.py <yaml_file>")
+    sys.exit(1)
+
+# Get the YAML file path from the command-line argument
+yaml_file_path = sys.argv[1]
+
+def yaml_parser(yaml_file_path):    
+    # Read the contents of the specified YAML file
+    try:
+        with open(yaml_file_path, 'r') as file:
+            yaml_data = file.read()
+    except FileNotFoundError:
+        print(f"Error: File '{yaml_file_path}' not found.")
+        sys.exit(1)
+
+    # Parse the YAML data
+    try:
+        data = yaml.safe_load(yaml_data)
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}")
+        sys.exit(1)
+    return data
+
+
+def get_promql_from_yaml_parser(data):    
+    # Extract variables from the 'spec' element
+    spec = data.get('spec', {})
+
+    # Get all keys in 'spec' before 'metric'
+    variables = {}
+    for key, value in spec.items():
+        if key == 'metric':
+            query = value
+            break
+        variables[key] = value
+
+    # Replace placeholders in 'query' with corresponding variables
+    for key, value in variables.items():
+        placeholder = f'{{{key}}}'
+        query = query.replace(placeholder, f'{{{value}}}')
+    return query
