365b96044b53293e3c1fa593303d376252cee5ba
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2023-11-13 12:38:53 +0000

    towrds-rating-docker,sh update

--- towards-rating-docker.sh
+++ towards-rating-docker.sh
@@ -3,8 +3,6 @@
 # Start the containers
 docker-compose up -d
 
-# Wait for the containers to start properly (you may need to adjust the sleep time based on your setup)
-sleep 4
 
 # Retrieve the value of RULES_FOLDER from the rules_path.env file
 source config.env
