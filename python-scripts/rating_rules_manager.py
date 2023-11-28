0805df16d6c10d7e7e35a01c476bdb9fd2c1eec6
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2023-11-28 16:15:42 +0000

    source code updated

--- python-scripts/rating_rules_manager.py
+++ python-scripts/rating_rules_manager.py
@@ -57,80 +57,7 @@ if args.instance:
 
 if args.update:
     yaml_file_path = args.update
-
-    try:
-        with open(yaml_file_path, 'r') as file:
-            yaml_data = file.read()
-    except FileNotFoundError:
-        print(f"Error: File '{yaml_file_path}' not found.")
-        
-
-    # Parse the YAML data
-    try:
-        data = yaml.safe_load(yaml_data)
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML file: {e}")
-
-    # Extract variables from the 'spec' element
-    spec = data.get('spec', {})
-
-    # Get all keys in 'spec' before 'metric'
-    variables = {}
-    for key, value in spec.items():
-        if key == 'metric':
-            query = value
-            break
-        variables[key] = value
-
-    # Replace placeholders in 'query' with corresponding variables
-    for key, value in variables.items():
-        placeholder = f'{{{key}}}'
-        query = query.replace(placeholder, f'{{{value}}}')
-
- 
-
-    # Define the Prometheus API URL
-    prometheus_url = 'http://localhost:9090/api/v1/query'
-
-    # Construct the request parameters
-    params = {
-
-        'query' : query_expression,
-
-    }
-
-    # Send the HTTP GET request to Prometheus
-    response = requests.get(prometheus_url, params=params)
-
-    table_name = "metric_data"
-    columns = ['metric_name','job_name','metric_time','value']
-    # Check if the request was successful (HTTP status code 200)
-    if response.status_code == 200:
-        result = response.json()
-        if result['data']['result']:
-            print(yaml_file_path)
-            print(result)
-            for item in result['data']['result']:
-                #print(item)
-                #job = item['metric']['job']
-                job = item['metric']
-                values = item['value']
-                if list_of_list(values):    
-                    for val in values:
-                        all_data = [query_expression,"",val[0],val[1]]
-                        insert_into_table(table_name,columns, all_data)
-                        
-                        
-                else:
-                    all_data = [query_expression,"",values[0],values[1]]
-                    insert_into_table(table_name, columns, all_data)
-                    
-
-        else : 
-            print(f" {yaml_file_path} : rules are not applicable")        
-    else:
-        print(f"Failed to execute query. Status code: {response.status_code}")
-
+    start_rating(yaml_file_path)
 
 
 if args.rm:
@@ -187,159 +114,16 @@ if args.add:
     else:
         print(f"Error: The specified file {yaml_file_path} does not exist.")
 
-    # Read the contents of the YAML file
-    try:
-        with open(yaml_file_path, 'r') as file:
-            yaml_data = file.read()
-    except FileNotFoundError:
-        print(f"Error: File '{yaml_file_path}' not found.")
-        
+    start_rating(yaml_file_path)
 
-    # Parse the YAML data
-    try:
-        data = yaml.safe_load(yaml_data)
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML file: {e}")
-
-    # Extract variables from the 'spec' element
-    spec = data.get('spec', {})
-
-    # Get all keys in 'spec' before 'metric'
-    variables = {}
-    for key, value in spec.items():
-        if key == 'metric':
-            query = value
-            break
-        variables[key] = value
-
-    # Replace placeholders in 'query' with corresponding variables
-    for key, value in variables.items():
-        placeholder = f'{{{key}}}'
-        query = query.replace(placeholder, f'{{{value}}}')
-
- 
-
-    # Define the Prometheus API URL
-    prometheus_url = 'http://localhost:9090/api/v1/query'
-
-    # Construct the request parameters
-    params = {
-
-        'query' : query_expression,
-
-    }
-
-    # Send the HTTP GET request to Prometheus
-    response = requests.get(prometheus_url, params=params)
-
-    table_name = "metric_data"
-    columns = ['metric_name','job_name','metric_time','value']
-    # Check if the request was successful (HTTP status code 200)
-    if response.status_code == 200:
-        result = response.json()
-        if result['data']['result']:
-            print(yaml_file_path)
-            print(result)
-            for item in result['data']['result']:
-                #print(item)
-                #job = item['metric']['job']
-                job = item['metric']
-                values = item['value']
-                if list_of_list(values):    
-                    for val in values:
-                        all_data = [query_expression,"",val[0],val[1]]
-                        insert_into_table(table_name,columns, all_data)
-                        
-                        
-                else:
-                    all_data = [query_expression,"",values[0],values[1]]
-                    insert_into_table(table_name, columns, all_data)
-                    
-
-        else : 
-            print(f" {yaml_file_path} : rules are not applicable")        
-    else:
-        print(f"Failed to execute query. Status code: {response.status_code}")
-
-
+    
 
 if args.folder_path and not args.add and not args.rm and not args.update:
 
     for filename in os.listdir(folder_path):
         if filename.endswith('.yaml'):
             yaml_file_path = os.path.join(folder_path, filename)
+            start_rating(yaml_file_path)
             
-            # Read the contents of the YAML file
-            try:
-                with open(yaml_file_path, 'r') as file:
-                    yaml_data = file.read()
-            except FileNotFoundError:
-                print(f"Error: File '{yaml_file_path}' not found.")
-                continue
-
-            # Parse the YAML data
-            try:
-                data = yaml.safe_load(yaml_data)
-            except yaml.YAMLError as e:
-                print(f"Error parsing YAML file: {e}")
-                continue
-
-            # Extract variables from the 'spec' element
-            spec = data.get('spec', {})
-
-            # Get all keys in 'spec' before 'metric'
-            variables = {}
-            for key, value in spec.items():
-                if key == 'metric':
-                    query = value
-                    break
-                variables[key] = value
-
-            # Replace placeholders in 'query' with corresponding variables
-            for key, value in variables.items():
-                placeholder = f'{{{key}}}'
-                query = query.replace(placeholder, f'{{{value}}}')
-
-            # Define the Prometheus API URL
-            prometheus_url = 'http://localhost:9090/api/v1/query'
-
-            # Construct the request parameters
-            params = {
-
-                'query' : query_expression,
-
-            }
-
-            # Send the HTTP GET request to Prometheus
-            response = requests.get(prometheus_url, params=params)
-
-            table_name = "metric_data"
-            columns = ['metric_name','job_name','metric_time','value']
-            # Check if the request was successful (HTTP status code 200)
-            if response.status_code == 200:
-                result = response.json()
-                if result['data']['result']:
-                    print(filename)
-                    print(result)
-                    for item in result['data']['result']:
-                        #print(item)
-                        #job = item['metric']['job']
-                        job = item['metric']
-                        values = item['value']
-                        if list_of_list(values):    
-                            for val in values:
-                                all_data = [query_expression,"",val[0],val[1]]
-                                insert_into_table(table_name,columns, all_data)
-                                
-                                
-                        else:
-                            all_data = [query_expression,"",values[0],values[1]]
-                            insert_into_table(table_name, columns, all_data)
-                            
-
-                else : 
-                    print(f" {filename} : rules are not applicable")        
-            else:
-                print(f"Failed to execute query. Status code: {response.status_code}")
                 
 
