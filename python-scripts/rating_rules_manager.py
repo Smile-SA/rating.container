b663c9df1d9c5d445f999630422a5ed660d25fa8
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2024-02-15 14:29:43 +0000

    src update

--- python-scripts/rating_rules_manager.py
+++ python-scripts/rating_rules_manager.py
@@ -82,7 +82,7 @@ if args.rm:
     
     variables = {}
     for key, value in spec.items():
-        if key == 'metric_name':
+        if key == 'name':
             metric_name = value
             break
 
