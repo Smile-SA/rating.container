d227070382144c696dc84f9c0322e8adeb163921
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2024-02-15 16:22:07 +0000

    init_project.sh update

--- init_project.sh
+++ init_project.sh
@@ -6,6 +6,8 @@ chmod +x python-scripts/rating_rules_manager.py
 chmod +x python-scripts/rating_results_to_json.py
 chmod +x python-scripts/test_rating_results.py
 chmod +x python-scripts/create_table.py
+cp prometheus.yml /etc/prometheus
+cp node_exporter.yml /etc/prometheus
 
 # Run the create_table.py script
 ./python-scripts/create_table.py
