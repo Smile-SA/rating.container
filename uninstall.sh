a10c2a05f9f53da413f9ae7ffc5cff10e39f3396
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2024-06-10 12:44:02 +0000

    spaces corrected

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-# Stop and remove all containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+# List of container names as specified in the docker-compose file
+containers=("prometheus" "node-exporter" "grafana" "timescaledb")
 
-# Remove all images
-docker rmi $(docker images -q)
+# Stop and remove specified containers
+for container in "${containers[@]}"; do
+  if [ $(docker ps -a -q -f name="$container") ]; then
+    echo "Stopping container: $container"
+    docker stop $container
+    echo "Removing container: $container"
+    docker rm $container
+  else
+    echo "Container $container not found"
+  fi
+done
+
+# List of images as specified in the docker-compose file
+images=("quay.io/prometheus/prometheus:v2.33.1" "quay.io/prometheus/node-exporter:v1.3.1" "grafana/grafana" "timescale/timescaledb-ha:pg14-latest")
+
+# Remove specified images
+for image in "${images[@]}"; do
+  if [ $(docker images -q "$image") ]; then
+    echo "Removing image: $image"
+    docker rmi $image
+  else
+    echo "Image $image not found"
+  fi
+done
 
 echo "uninstallation completed"
